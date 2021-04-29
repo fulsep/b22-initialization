5c3d6812a127a5fb7054fc945629ff27723f148d
fulsep <57238803+fulsep@users.noreply.github.com>
2021-04-29 17:01:09 +0700

    initial push

---
+++ index.js
@@ -0,0 +1,10 @@
+// const sum = require('./src/basic').sum
+// const div = require('./src/basic').div
+
+const { sum, div } = require('./src/basic');
+
+const result = sum(6, 2);
+const result2 = div(6, 2);
+
+console.log(result);
+console.log(result2);
---
+++ src/basic.js
@@ -0,0 +1,29 @@
+// exports.sum = function (a, b){
+//   return a + b
+// }
+
+// exports.div = function (a, b){
+//   return a / b
+// }
+
+// function sum(a, b){
+//   return a + b
+// }
+
+// function div(a,b){
+//   return a/b
+// }
+
+// module.exports = {
+//   sum, // = sum:sum
+//   div
+// }
+
+module.exports = {
+  sum: function (a, b) {
+    return a + b;
+  },
+  div: function (a, b) {
+    return a / b;
+  }
+};
